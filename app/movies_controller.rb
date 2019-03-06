9022532ab9c9484efaea1ee5964977229b55baca
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-03-06 02:28:08 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -84,7 +84,7 @@ end
 
 def can_update_multiple_items_at_once
   # Change title of all movies to "A Movie"
- 5.times do |i|
+  5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
   Movie.all.each{|movie|
